35014266dafbc3d94a6139123442facd2f1ac261
Precious Humphrey <phumphrey23@gmail.com>
2018-11-10 18:16:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
+        resp.write "#{item.price}\n"
       end
       resp.status = 200
     else
